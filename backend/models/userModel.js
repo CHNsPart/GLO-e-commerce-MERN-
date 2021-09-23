40475ccc54e9a712a0b29b83053f348bcdce530e
CHNsPart <imchn24@gmail.com>
2021-09-23 15:49:03 +0600

    Models added

---
+++ backend/models/userModel.js
@@ -0,0 +1,29 @@
+import Mongoose from "mongoose"
+
+const userSchema = Mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    isAdmin: {
+        type: Boolean,
+        required: true,
+        default: false
+    },
+
+}, {
+    timestamps: true
+})
+
+const User = Mongoose.model("User", userSchema)
+
+export default User
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
 import colors from "colors"
+import ora from "ora"
 import connectDB from "./config/db.js"
 import products from "./data/product.js"
 
@@ -26,8 +27,30 @@ app.get("/api/products/:id", (req, res) => {
 
 const PORT = process.env.PORT || 5000
 
+
+const throbber = ora({
+  text: '',
+  spinner: {
+    frames: ['👾', '🤖', '👨‍💻', '🕵🏼‍♂️'],
+    interval: 200, // Optional
+  },
+}).start();
+
+ setTimeout(() => {
+  throbber.stop();
+}, 1000 * 10) 
+
+
+// Simulating some asynchronous work for 10 seconds...
+
+
 app.listen(PORT, 
-    console.log(`Server is on live in`.brightMagenta.bold+`\n@...@\n#...#\n%...%\n#...#\n&...&\n`.green+`{ ${process.env.NODE_ENV} }`.brightGreen.bold+` mode on port `.black.bold.dim.italic +
+    console.log(
+            `Server is on live in`.brightMagenta.bold+`\n@...@\n#...#\n%...%\n#...#\n&...&\n`.green+`{ ${process.env.NODE_ENV} }`.brightGreen.bold+` mode on port `.black.bold.dim.italic +
     `{ ${PORT} }`.brightMagenta+
     `\n\n ------------- `.america+`CHNsBOT`.trap+ ` -------------\n`.america)
 )
+
+setTimeout(() => {
+        throbber.stop();
+    }, 1000 * 2)
